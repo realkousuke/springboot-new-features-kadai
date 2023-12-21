7464de53ee7468ffd4e818f1e959631b7a8f56ae
Kosuke516 <realkousuke5@outlook.jp>
2023-12-21 19:54:16 +0900

    再提出

---
+++ src/main/resources/static/js/stripe.js
@@ -0,0 +1,8 @@
+const stripe = Stripe('pk_test_51OLIkfCQUPdue0pucRwjiyC0FSiaYiIeI0xcwLYo1dVEha7TmCEOaNR7cHXCLPBdCtgoXR0JtSvoAH71oajOP1P600BbEUIzeU');
+ const paymentButton = document.querySelector('#paymentButton');
+ 
+ paymentButton.addEventListener('click', () => {
+   stripe.redirectToCheckout({
+     sessionId: sessionId
+   })
+ });
